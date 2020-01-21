f1f9c6088f7e2c7f37ed4f7ecba98971f272f8bc
Oleksandr Palokha <palokha.oa@gmail.com>
2020-01-21 13:23:33 +0200

    Added srv, moved requests logic, changed requests

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,39 +1,22 @@
+const apiUrl = 'http://localhost:5000';
+
 export const httpService = {
-    recognizeByLyrics: lyrics => fetch(`https://api.audd.io/?method=findLyrics&q=${lyrics}`)
-        .then(res => res.json())
-        .then(body => (body.result || []).map(res => ({
-            title: res.full_title,
-        })))
-        .catch(() => []),
+    recognizeByLyrics: lyrics => fetch(`${apiUrl}/by_lyrics?q=${lyrics}`)
+        .then(res => res.json()),
     recognizeByClip: blob => {
         const body = new FormData();
         body.set('file', blob);
-        body.set('return', 'deezer');
-        body.set('method', 'recognize');
-        return fetch('https://api.audd.io/', {
+        return fetch(`${apiUrl}/by_clip`, {
             method: 'POST',
             body,
-        })
-            .then(res => res.json())
-            .then(body => (body || []).map(res => ({
-                title: res.full_title,
-                deezer_id: res.deezer.id,
-                cover: res.deezer.album.cover,
-            })))
-            .catch(() => []);
+        }).then(res => res.json());
     },
     recognizeByHum: blob => {
         const body = new FormData();
         body.set('file', blob);
-        body.set('method', 'recognizeWithOffset');
-        return fetch('https://api.audd.io/', {
-                method: 'POST',
-                body,
-            })
-            .then(res => res.json())
-            .then(body => (body.result.list || []).map(res => ({
-                title: `${res.title} by ${res.artist}`,
-            })))
-            .catch(() => []);
+        return fetch(`${apiUrl}/by_hum`, {
+            method: 'POST',
+            body,
+        }).then(res => res.json());
     }
 };
