61dd867743c6cea2eeb2574ae6c0bd217668050c
torstein_mp <p98avlo@gmail.com>
2020-01-25 21:57:42 +0200

    handle no audio src

--- src/app/components/AudioPlayer/AudioPlayer.js
+++ src/app/components/AudioPlayer/AudioPlayer.js
@@ -1,49 +1,73 @@
-import React, { useState, useRef } from 'react';
-import styles from './AudioPlayer.module.css';
+import React, { useState, useRef } from "react";
+import styles from "./AudioPlayer.module.css";
 
-const AudioPlayer = ({src, title}) => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isStarted, setIsStarted] = useState(false);
-    const [progress, setProgress] = useState(0);
+const AudioPlayer = ({ src, title }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-    const audioRef = useRef(null);
-    const rangeRef = useRef(null);
+  const audioRef = useRef(null);
+  const rangeRef = useRef(null);
 
-    const togglePause = () => {
-        if (isPlaying) {
-            audioRef.current.pause();
-        } else {
-            setIsStarted(true);
-            audioRef.current.play();
-        }
-        setIsPlaying(!isPlaying);
+  const togglePause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      setIsStarted(true);
+      audioRef.current.play();
     }
+    setIsPlaying(!isPlaying);
+  };
 
-    const updateProgress = () => {
-        const progress = audioRef.current.currentTime / audioRef.current.duration;
-        setProgress(progress);
-        const rangeShadowLength = rangeRef.current.clientWidth * progress
-        rangeRef.current.style.setProperty('--shadow', `inset ${rangeShadowLength}px 0 #0435FD`);
-    }
+  const updateProgress = () => {
+    const progress = audioRef.current.currentTime / audioRef.current.duration;
+    setProgress(progress);
+    const rangeShadowLength = rangeRef.current.clientWidth * progress;
+    rangeRef.current.style.setProperty(
+      "--shadow",
+      `inset ${rangeShadowLength}px 0 #0435FD`
+    );
+  };
 
-    const seek = (e) => {
-        audioRef.current.currentTime = e.target.value * audioRef.current.duration;
-    }
+  const seek = e => {
+    audioRef.current.currentTime = e.target.value * audioRef.current.duration;
+  };
 
-    const rangeClassName = `${styles.range} ${isStarted ? '' : styles.hidden}`
+  const rangeClassName = `${styles.range} ${isStarted ? "" : styles.hidden}`;
 
-    return src
-        ? <div className={styles.wrapper}>
-            <button variant="secondary" className={styles.button} onClick={() => togglePause()}>
-                <img className={styles.icon} src={isPlaying ? '/images/pause.svg' : '/images/play.svg'}></img>
-            </button>
-            <div className={styles.labelProgressWrapper}>
-                <label className={styles.label}>{title}</label>
-                <input type="range" ref={rangeRef} className={rangeClassName} value={progress} onChange={seek} min="0" max="1" step="0.001"></input>
-            </div>
-            <audio src={src} ref={audioRef} onTimeUpdate={updateProgress}></audio>
-        </div>
-        : <span>{title}</span>;
-}
+  return src ? (
+    <div className={styles.wrapper}>
+      <button
+        variant="secondary"
+        className={styles.button}
+        onClick={() => togglePause()}
+      >
+        <img
+          className={styles.icon}
+          src={isPlaying ? "/images/pause.svg" : "/images/play.svg"}
+        />
+      </button>
+      <div className={styles.labelProgressWrapper}>
+        <label className={styles.label}>{title}</label>
+        <input
+          type="range"
+          ref={rangeRef}
+          className={rangeClassName}
+          value={progress}
+          onChange={seek}
+          min="0"
+          max="1"
+          step="0.001"
+        />
+      </div>
+      <audio src={src} ref={audioRef} onTimeUpdate={updateProgress} />
+    </div>
+  ) : (
+    <div>
+      <p>{title}</p>
+      <span>Sorry, i couldn`t found audio</span>
+    </div>
+  );
+};
 
 export default AudioPlayer;
--- src/app/components/ResultsPage/ResultsPage.js
+++ src/app/components/ResultsPage/ResultsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.scss";
 import AudioPlayer from "../AudioPlayer/AudioPlayer";
-import styles from './ResultsPage.module.css';
+import styles from "./ResultsPage.module.css";
 
 const ResultsPage = ({
   toggleGameStatus,
@@ -13,27 +13,22 @@ const ResultsPage = ({
     <section className="lyricsPage">
       <div className="lyricsPage__dialog">
         <img className="lyricsPage__dialog--gif" src="/images/ai.gif" />
-        <div className="lyricsPage__dialog--text" style={{width:"unset"}}>
+        <div className="lyricsPage__dialog--text" style={{ width: "unset" }}>
           {successSong ? (
             <>
               <h4>You lose!</h4>
               <span>
-                I guessed the song. Time to understand that I can guess any song.
+                I guessed the song. Time to understand that I can guess any
+                song.
               </span>
               <span>Here are my tries:</span>
             </>
           ) : (
             <>
               <h4>You win this time!</h4>
-              <span>
-                I didn’t guess the song. Don’t be so happy, human.
-              </span>
-              <span>
-                Maybe your request was too complicated.
-              </span>
-              <span>
-                Here is how I was trying to guess the song:
-              </span>
+              <span>I didn’t guess the song. Don’t be so happy, human.</span>
+              <span>Maybe your request was too complicated.</span>
+              <span>Here is how I was trying to guess the song:</span>
             </>
           )}
         </div>
@@ -42,24 +37,43 @@ const ResultsPage = ({
       <div className="lyricsPage__inputVariants">
         {wrongSongs.length > 0 && (
           <div className={`lyricsPage__borderedDiv ${styles.guesses}`}>
-            <p><b>Wrong tries:</b></p>
-            {wrongSongs.map((song, index) => (
-              <AudioPlayer key={index} src={song.preview} title={song.title} />
-            ))}
+            <p>
+              <b>Wrong tries:</b>
+            </p>
+            <ul>
+              {wrongSongs.map((song, index) => (
+                <li>
+                  <AudioPlayer
+                    key={index}
+                    src={song.preview}
+                    title={song.title}
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
         )}
         {successSong && (
           <div className={`lyricsPage__borderedDiv ${styles.guesses}`}>
-            <p><b>Success try:</b></p>
-            <AudioPlayer src={successSong.preview} title={successSong.title} />
+            <p>
+              <b>Success try:</b>
+            </p>
+            <ul>
+              <li>
+                <AudioPlayer
+                  src={successSong.preview}
+                  title={successSong.title}
+                />
+              </li>
+            </ul>
           </div>
         )}
       </div>
       <div className="lyricsPage__inputVariants">
-      <button variant="secondary" onClick={toggleGameStatus}>
-        Quit
-      </button>
-      <button onClick={startNewRound}>Play again</button>
+        <button variant="secondary" onClick={toggleGameStatus}>
+          Quit
+        </button>
+        <button onClick={startNewRound}>Play again</button>
       </div>
     </section>
   );
